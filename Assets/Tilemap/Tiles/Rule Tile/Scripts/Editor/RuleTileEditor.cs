b3e9e8add8c1d299bbae67c0209bf5a687b38385
Bryan Alvarado <bryan.alvarado@gmail.com>
2018-05-15 11:20:34 -0600

    Add animated tile example

--- Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/RuleTileEditor.cs
+++ Assets/Tilemap/Tiles/Rule Tile/Scripts/Editor/RuleTileEditor.cs
@@ -184,7 +184,10 @@ namespace UnityEditor
 						}
 						if (Event.current.type == EventType.MouseDown && r.Contains(Event.current.mousePosition))
 						{
-							tilingRule.m_Neighbors[index] = (RuleTile.TilingRule.Neighbor) (((int)tilingRule.m_Neighbors[index] + 1) % 3);
+                            int change = 1;
+						    if (Event.current.button == 1)
+								change = -1;
+							tilingRule.m_Neighbors[index] = (RuleTile.TilingRule.Neighbor) (((int)tilingRule.m_Neighbors[index] + change) % 3);
 							GUI.changed = true;
 							Event.current.Use();
 						}
@@ -275,7 +278,7 @@ namespace UnityEditor
 			{
 				GUI.Label(new Rect(rect.xMin, y, k_LabelWidth, k_SingleLineHeight), "Size");
 				EditorGUI.BeginChangeCheck();
-				int newLength = EditorGUI.IntField(new Rect(rect.xMin + k_LabelWidth, y, rect.width - k_LabelWidth, k_SingleLineHeight), tilingRule.m_Sprites.Length);
+				int newLength = EditorGUI.DelayedIntField(new Rect(rect.xMin + k_LabelWidth, y, rect.width - k_LabelWidth, k_SingleLineHeight), tilingRule.m_Sprites.Length);
 				if (EditorGUI.EndChangeCheck())
 					Array.Resize(ref tilingRule.m_Sprites, Math.Max(newLength, 1));
 				y += k_SingleLineHeight;
